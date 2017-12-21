9d4ba2703091ed969279db107726048ec1fca5e1
yury <yury.alvarez.tapia@gmail.com>
2017-12-21 18:16:19 -0300

    Tesis Final ADMPROP Bootcamp G8 con seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,6 @@
 
 Propiedad.destroy_all
 Recinto.destroy_all
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
